aa2c1e1805bb9f8ff48f182cd53ff55f9bbbb83a
Christian Smith Mantas <casm202@gmail.com>
2023-06-24 16:00:53 +0200

    Update: header and slanted container components

--- packages/ui-components/src/components/Slanted Container/index.tsx
+++ packages/ui-components/src/components/Slanted Container/index.tsx
@@ -5,10 +5,11 @@ interface SContainerProps {
 	children: React.ReactNode | JSX.Element;
 	color?: string;
 	rotation?: 'left' | 'right';
+	shadow?: boolean;
 }
 
-export const SlantedContainer = ({ children, color = '#ccfbf1', rotation }: SContainerProps) => (
-	<StyledSlantedContainer color={color} rotation={rotation}>
+export const SlantedContainer = ({ children, color = '#ccfbf1', rotation, shadow = false }: SContainerProps) => (
+	<StyledSlantedContainer color={color} rotation={rotation} shadow={shadow}>
 		{children}
 	</StyledSlantedContainer>
 );
--- packages/ui-components/src/components/Slanted Container/styles.ts
+++ packages/ui-components/src/components/Slanted Container/styles.ts
@@ -3,9 +3,10 @@ import styled from "@emotion/styled";
 interface StyledProps {
 	color?: string;
 	rotation?: 'left' | 'right';
+	shadow?: boolean;
 }
 
-export const StyledSlantedContainer = styled.div(({ color, rotation }: StyledProps) => ({
+export const StyledSlantedContainer = styled.div(({ color, rotation, shadow }: StyledProps) => ({
 	background: color,
 	padding: '1.5rem',
 	transform: rotation === 'left' ?
@@ -18,5 +19,6 @@ export const StyledSlantedContainer = styled.div(({ color, rotation }: StyledPro
 		"skewX(2deg) skewY(-1.75deg)" :
 		"skewX(-2deg) skewY(1.75deg)"
 		,
-	}
+	},
+	boxShadow: shadow ? '0 0 30px -10px grey' : '',
 }));
