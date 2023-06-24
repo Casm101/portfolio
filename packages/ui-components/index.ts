80c6e3b4290032d052fab3c168b82f1bb428004c
Christian Smith Mantas <casm202@gmail.com>
2023-06-24 16:01:39 +0200

    Update: UI library documentation and exports

--- packages/ui-components/index.ts
+++ packages/ui-components/index.ts
@@ -1,7 +1,9 @@
 export * from './src/components/Button';
 export * from './src/components/Header';
+export * from './src/components/InfiniteScroller';
 export * from './src/components/Link';
 export * from './src/components/Slanted Container';
 export * from './src/components/Trapezoid Container';
+export * from './src/components/Testimonial';
 
 export * from './src/theme';
